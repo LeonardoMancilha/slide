09ea273e1f3b74922a8cce52d266067c30fe1b42
Leonardo Machado <leonardomachado.sjc@gmail.com>
2024-07-08 18:18:41 -0300

    calculo de distancias quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -2,18 +2,37 @@ export class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+        this.distancias = {
+            finalPosition: 0,
+            startX: 0,
+            movement: 0
+        }
     }
 
     onStart(event) {
         event.preventDefault();
+        this.distancias.startX = event.clientX;
         this.wrapper.addEventListener('mousemove', this.onMove);
     }
 
+    updatePosition(clientX) {
+        this.distancias.movement = (this.distancias.startX - clientX) * 1.5;
+        return this.distancias.finalPosition - this.distancias.movement;
+    }
+
     onMove(event) {
+        const finalPosition = this.updatePosition(event.clientX);
+        this.moveSlide(finalPosition);
+    }
+
+    moveSlide(distanciaX) {
+        this.distancias.movePosition = distanciaX;
+        this.slide.style.transform = `translate3d(${distanciaX}px, 0, 0)`;
     }
 
     onEnd() {
         this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.distancias.finalPosition = this.distancias.movePosition;
     }
 
     addSlideEvents() {
